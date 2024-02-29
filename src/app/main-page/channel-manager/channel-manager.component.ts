47c2566ad16973ca9e0f33a69e14e4af9522bd72
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2024-02-29 13:33:48 +0100

    created primary structure

---
+++ src/app/main-page/channel-manager/channel-manager.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-channel-manager',
+  standalone: true,
+  imports: [],
+  templateUrl: './channel-manager.component.html',
+  styleUrl: './channel-manager.component.scss'
+})
+export class ChannelManagerComponent {
+
+}
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { HeadComponent } from './head/head.component';
+import { ChatContainerComponent } from './chat-container/chat-container.component';
+import { ThreadComponent } from './thread/thread.component';
+import { ChannelManagerComponent } from './channel-manager/channel-manager.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [
+    HeadComponent,
+    ChatContainerComponent,
+    ThreadComponent,
+    ChannelManagerComponent
+  ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
