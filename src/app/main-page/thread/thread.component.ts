e8c96966e7f86e0758a828b71256039ce1953415
Meran Osman <meran-osman@gmx.de>
2024-03-02 19:48:50 +0100

    thread header

--- src/app/main-page/thread/thread.component.ts
+++ src/app/main-page/thread/thread.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ThreadHeaderComponent } from './thread-header/thread-header.component';
 
 @Component({
   selector: 'app-thread',
   standalone: true,
-  imports: [],
+  imports: [ThreadHeaderComponent],
   templateUrl: './thread.component.html',
   styleUrl: './thread.component.scss'
 })
