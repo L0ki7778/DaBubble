d2e831197b7a307f56080ca0a067d0f027692c55
Meran Osman <meran-osman@gmx.de>
2024-03-03 14:58:09 +0100

    overlay add member

--- src/app/main-page/overlay/overlay.component.ts
+++ src/app/main-page/overlay/overlay.component.ts
@@ -4,6 +4,7 @@ import { ChatOverlayComponent } from './chat-overlay/chat-overlay.component';
 import { WorkspaceOverlayComponent } from './workspace-overlay/workspace-overlay.component';
 import { CommonModule } from '@angular/common';
 import { DropdownMenuComponent } from './dropdown-menu/dropdown-menu.component';
+import { AddMemberOverlayComponent } from './add-member-overlay/add-member-overlay.component';
 
 @Component({
   selector: 'app-overlay',
@@ -12,7 +13,8 @@ import { DropdownMenuComponent } from './dropdown-menu/dropdown-menu.component';
     ChatOverlayComponent,
     WorkspaceOverlayComponent,
     CommonModule,
-    DropdownMenuComponent
+    DropdownMenuComponent,
+    AddMemberOverlayComponent
   ],
   templateUrl: './overlay.component.html',
   styleUrl: './overlay.component.scss'
@@ -21,5 +23,6 @@ export class OverlayComponent {
   overlay = inject(OverlayService);
   chatOverlay = this.overlay.chatOverlay;
   workspaceOverlay = this.overlay.workspaceOverlay;
-  isDropdownMenuVisible = this.overlay.isDropdownMenuVisible;  
+  isDropdownMenuVisible = this.overlay.isDropdownMenuVisible;
+  addMemberOverlay = this.overlay.addMemberOverlay;
 }
