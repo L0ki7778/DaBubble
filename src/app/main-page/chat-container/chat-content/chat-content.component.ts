cbba7ba95b0f107ba096f4f1d03b3fbff78568d1
PascalGajewski <121040841+PascalGajewski@users.noreply.github.com>
2024-03-04 18:01:17 +0100

    changed img srcs in html

--- src/app/main-page/chat-container/chat-content/chat-content.component.ts
+++ src/app/main-page/chat-container/chat-content/chat-content.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ChatMessageComponent } from './chat-message/chat-message.component';
 
 @Component({
   selector: 'app-chat-content',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ChatMessageComponent],
   templateUrl: './chat-content.component.html',
   styleUrl: './chat-content.component.scss'
 })
