c301111e54ef34918455a944bbd178feb322a82d
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2024-03-04 11:13:18 +0100

    fixed thread head

--- src/app/main-page/workspace/workspace.component.ts
+++ src/app/main-page/workspace/workspace.component.ts
@@ -1,12 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { OverlayService } from '../../services/overlay.service';
 import { WorkspaceDropdownComponent } from './workspace-dropdown/workspace-dropdown.component';
+import { WorkspaceHeaderComponent } from './workspace-header/workspace-header.component';
 
 @Component({
   selector: 'app-workspace',
   standalone: true,
   imports: [
-    WorkspaceDropdownComponent
+    WorkspaceDropdownComponent,
+    WorkspaceHeaderComponent
   ],
   templateUrl: './workspace.component.html',
   styleUrl: './workspace.component.scss'
