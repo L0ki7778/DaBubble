719451b5aea72c320e8df6b2e6e8ec0f7af0f57d
AntonOsipov99 <antonosipov@gmx.de>
2024-03-05 20:22:03 +0100

    add firestore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,9 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export function HttpLoaderFactory(httpClient: HttpClient) {
   return  new  TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
@@ -30,6 +33,6 @@ export const appConfig: ApplicationConfig = {
       HttpClientModule,
       TranslateModule.forRoot(provideTranslation())
     ]),
-    provideRouter(routes),
+    provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"dabubble-adb2f","appId":"1:213302941076:web:08da4ba8e6b88095beab81","storageBucket":"dabubble-adb2f.appspot.com","apiKey":"AIzaSyCoTeNdkTPlU28jZjYFwkFArrVMS7fBUAY","authDomain":"dabubble-adb2f.firebaseapp.com","messagingSenderId":"213302941076"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())),
   ]
 };
