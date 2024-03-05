330caa850f5eee17405091055ca1504851a33091
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2024-03-05 13:40:11 +0100

    workspace-shifting works

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, ElementRef, ViewChild, inject, signal } from '@angular/core';
 import { HeadComponent } from './head/head.component';
 import { ChatContainerComponent } from './chat-container/chat-container.component';
 import { ThreadComponent } from './thread/thread.component';
@@ -8,6 +8,7 @@ import { OverlayComponent } from './overlay/overlay.component';
 import { CommonModule } from '@angular/common';
 import { ThreadHeaderComponent } from './thread/thread-header/thread-header.component';
 import { BooleanValueService } from '../services/boolean-value.service';
+import { CloseWorkspaceComponent } from './workspace/close-workspace/close-workspace.component';
 
 @Component({
   selector: 'app-main-page',
@@ -19,7 +20,8 @@ import { BooleanValueService } from '../services/boolean-value.service';
     ThreadComponent,
     WorkspaceComponent,
     OverlayComponent,
-    ThreadHeaderComponent
+    ThreadHeaderComponent,
+    CloseWorkspaceComponent
   ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
@@ -27,14 +29,37 @@ import { BooleanValueService } from '../services/boolean-value.service';
 export class MainPageComponent {
   overlayService = inject(OverlayService);
   booleanService = inject(BooleanValueService);
+  workspaceOpen= true;
+  @ViewChild('WorkspaceComponent',{read : ElementRef}) workspaceComponentRef: ElementRef  | undefined;
 
-  overlay = this.overlayService.overlay;
+  overlay:any;
   viewThread = this.booleanService.viewThread;
 
 
   constructor() {
     this.overlayService.overlaySubject.subscribe(() => {
       this.overlay = this.overlayService.overlay;
-    })
+    });
+  }
+
+  handleWorkspace() {
+    if(this.workspaceComponentRef) {
+      const workspaceElement :HTMLElement = this.workspaceComponentRef.nativeElement;
+     if(workspaceElement.style.width === '0px'){
+      this.workspaceOpen = true;
+       workspaceElement.style.width = '';
+       workspaceElement.style.margin = '';
+       workspaceElement.style.opacity = '';
+       setTimeout(()=>{
+         workspaceElement.style.minWidth = '';
+       },250)
+     }else{
+       this.workspaceOpen = false;
+       workspaceElement.style.width = '0';
+       workspaceElement.style.minWidth = 'unset';
+       workspaceElement.style.margin = '0 -2rem';
+       workspaceElement.style.opacity = '0';
+     }
+    }
   }
 }
---
+++ src/app/main-page/workspace/close-workspace/close-workspace.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-close-workspace',
+  standalone: true,
+  imports: [],
+  templateUrl: './close-workspace.component.html',
+  styleUrl: './close-workspace.component.scss'
+})
+export class CloseWorkspaceComponent {
+@Input() workspaceOpen = true;
+}
