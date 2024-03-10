6993e6877d749ff689e70d4a86c4370ac1a88f2f
PascalGajewski <121040841+PascalGajewski@users.noreply.github.com>
2024-03-10 18:11:33 +0100

    designed privacy-policy component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { StartPageComponent } from './start-page/start-page.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     { path: '', component: StartPageComponent },
     { path: 'main-page', component: MainPageComponent },
-    { path: 'imprint', component: ImprintComponent }
+    { path: 'imprint', component: ImprintComponent }, 
+    { path: 'privacy-policy', component: PrivacyPolicyComponent }
 ];
