6e6523932b08b3ed06eab7635a4885f5d032f28d
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2024-03-13 12:52:59 +0100

    implemented emojiPicker

--- src/app/main-page/chat-container/chat-container.component.ts
+++ src/app/main-page/chat-container/chat-container.component.ts
@@ -3,13 +3,38 @@ import { Component } from '@angular/core';
 import { ChatHeaderComponent } from './chat-header/chat-header.component';
 import { ChatContentComponent } from './chat-content/chat-content.component';
 import { ChatInputComponent } from './chat-input/chat-input.component';
+import { EditorModule } from '@tinymce/tinymce-angular';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-chat-container',
   standalone: true,
-  imports: [CommonModule, ChatHeaderComponent, ChatContentComponent, ChatInputComponent],
+  imports: [
+    CommonModule,
+    ChatHeaderComponent,
+    ChatContentComponent,
+    ChatInputComponent,
+    FormsModule,
+    EditorModule,
+    ReactiveFormsModule],
   templateUrl: './chat-container.component.html',
   styleUrl: './chat-container.component.scss'
 })
 export class ChatContainerComponent {
+  content = "";
+
+  chatForm : FormGroup = new FormGroup({});
+
+  ngOnInit(){
+    this.chatForm = new FormGroup({
+      message: new FormControl('')
+    })
+  }
+
+
+onSubmit(){
+console.log(this.chatForm.value)
+}
+
+
 }
