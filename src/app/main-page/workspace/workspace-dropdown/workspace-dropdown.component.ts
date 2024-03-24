3b7e7def20a03102be8d935a9b48d3b4a5460e9b
PascalGajewski <121040841+PascalGajewski@users.noreply.github.com>
2024-03-24 14:24:06 +0100

    added user specific view to workspace dropdown

--- src/app/main-page/workspace/workspace-dropdown/workspace-dropdown.component.ts
+++ src/app/main-page/workspace/workspace-dropdown/workspace-dropdown.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, ViewChild, inject } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { DirectMessagesService } from '../../../services/direct-messages.service';
 import { SelectionService } from '../../../services/selection.service';
+import { CollectionReference, Firestore, collection, doc, onSnapshot, query } from '@angular/fire/firestore';
 
 
 @Component({
@@ -21,11 +22,24 @@ export class WorkspaceDropdownComponent {
   @ViewChild('arrow') arrow: HTMLImageElement | undefined;
   selectionService = inject(SelectionService);
   authService: AuthService = inject(AuthService);
+  firestore = inject(Firestore);
   DMService: DirectMessagesService = inject(DirectMessagesService);
+  channelsRef: CollectionReference = collection(this.firestore, "channels");
+  private unsubscribeChannel: (() => void) | undefined;
   showList = false;
+  currentUserID = this.DMService.getLoggedInUserId();
+  filteredChannelNames: string[] = [];
 
-  constructor() { 
-   }
+  constructor() {
+    const q = query(this.channelsRef);
+    const unsubscribeChannel = onSnapshot(q, (querySnapshot) => {
+      this.filteredChannelNames = [];
+      querySnapshot.forEach((doc) => {
+        if(doc.data()['members'].includes(this.currentUserID))
+        this.filteredChannelNames.push(doc.data()['channelName'] as string);
+      });
+    });
+  }
 
 
   ngOnInit() {
@@ -33,12 +47,14 @@ export class WorkspaceDropdownComponent {
     this.checkName();
   }
 
-  sendChannelId(index: number){
+
+
+  sendChannelId(index: number) {
     this.selectionService.choosenChatTypeId.next(this.selectionService.channelIds[index]);
     this.selectionService.channelOrDM.next('channel');
   }
 
-  sendDMId(index: number){
+  sendDMId(index: number) {
     this.selectionService.choosenChatTypeId.next(this.selectionService.DMIds[index]);
     this.selectionService.channelOrDM.next('direct-message');
   }
