37e496ae0e31a8b3b13b6278249d44f29bdad948
PascalGajewski <121040841+PascalGajewski@users.noreply.github.com>
2024-03-24 12:12:43 +0100

    changed chat input

--- src/app/main-page/chat-container/chat-input/chat-input.component.ts
+++ src/app/main-page/chat-container/chat-input/chat-input.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, ViewChild, inject } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { EditorModule } from '@tinymce/tinymce-angular';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { DirectMessagesService } from '../../../services/direct-messages.service';
+import { SelectionService } from '../../../services/selection.service';
 
 
 @Component({
@@ -18,43 +19,50 @@ import { DirectMessagesService } from '../../../services/direct-messages.service
   templateUrl: './chat-input.component.html',
   styleUrl: './chat-input.component.scss'
 })
-export class ChatInputComponent {
+export class ChatInputComponent{
+
   DMService: DirectMessagesService = inject(DirectMessagesService);
-  characterCount=0;
-  maxCharacters=300;
-  characterSubscription? : Subscription;
+  selectionService: SelectionService = inject(SelectionService);
+  characterCount = 0;
+  maxCharacters = 300;
+  characterSubscription?: Subscription;
 
   chatForm: FormGroup = new FormGroup({});
   chatContent: string = '';
+
   ngOnInit() {
     this.newMessage();
   };
 
   async onSubmit(chatContent: string) {
-    const otherUserId = await this.DMService.getUserId(this.DMService.selectedUserName);
-    if (otherUserId) {
-      await this.DMService.addUserToDirectMessagesWithIds(otherUserId, chatContent);
-      await this.DMService.loadChatHistory();
-      this.chatContent = '';
-    } else {
-      console.error('Error getting user ID');
+    if (this.selectionService.channelOrDM.value === 'channel') {
+      console.log('send message to channel');
+      // muss noch implementiert werden
+    }
+    if (this.selectionService.channelOrDM.value === 'direct-message') {
+      const otherUserId = await this.DMService.getUserId(this.DMService.selectedUserName);
+      if (otherUserId) {
+        await this.DMService.addUserToDirectMessagesWithIds(otherUserId, chatContent);
+        await this.DMService.loadChatHistory();
+        this.chatContent = '';
+      } else {
+        console.error('Error getting user ID');
+      }
     }
   }
 
-  subscribeCharacters(){
+  subscribeCharacters() {
     this.characterSubscription = this.chatForm.get('message')?.valueChanges.subscribe((value) => {
       this.characterCount = value.length;
     })
   };
 
-
-  newMessage(){
+  newMessage() {
     this.chatForm = new FormGroup({
-      message: new FormControl('',[Validators.required, Validators.minLength(5), Validators.maxLength(300)]),
+      message: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(300)]),
     });
     this.subscribeCharacters();
   };
-
 
   // onSubmit() {
   //   console.log(this.chatForm.value);
