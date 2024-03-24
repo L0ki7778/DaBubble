f4ef49250d0d7664c90ca91c40e655d970c699e5
Meran Osman <meran-osman@gmx.de>
2024-03-24 19:39:12 +0100

    emoji-mart

--- src/app/main-page/chat-container/chat-content/chat-message/chat-message.component.ts
+++ src/app/main-page/chat-container/chat-content/chat-message/chat-message.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, inject } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, ViewChild, inject } from '@angular/core';
 import { OverlayService } from '../../../../services/overlay.service';
 import { ReactionBarComponent } from './reaction-bar/reaction-bar.component';
 import { BooleanValueService } from '../../../../services/boolean-value.service';
 import { Firestore, collection, doc, getDoc, getDocs } from '@angular/fire/firestore';
+import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 
 @Component({
   selector: 'app-chat-message',
   standalone: true,
-  imports: [CommonModule, ReactionBarComponent],
+  imports: [CommonModule, ReactionBarComponent, PickerComponent],
   templateUrl: './chat-message.component.html',
   styleUrl: './chat-message.component.scss',
 })
@@ -19,8 +20,11 @@ export class ChatMessageComponent {
 
   @Input() isOwnMessage: boolean = true;
   @Input() message: any;
+  @ViewChild('emoji') emoji: ElementRef | null = null;
+
 
   isHovered: boolean = false;
+  viewEmojiPicker: boolean = false;
   user: any = {};
 
   ngOnInit() {
@@ -55,5 +59,23 @@ export class ChatMessageComponent {
 
   showThread() {
     this.booleanService.viewThread.set(true);
+  }
+
+  showEmojiPicker(event: MouseEvent) {
+    event.stopPropagation();
+    this.viewEmojiPicker = true;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onclick(event: Event) {
+    if (this.emoji && this.emoji.nativeElement && this.emoji.nativeElement.contains(event.target)) {
+      return
+    } else {
+      this.viewEmojiPicker = false;
+    }
+  }
+
+  addEmoji(event: MouseEvent) {
+    const emoji = document.createElement('span');
   }
 }
--- src/app/main-page/chat-container/chat-content/chat-message/reaction-bar/reaction-bar.component.ts
+++ src/app/main-page/chat-container/chat-content/chat-message/reaction-bar/reaction-bar.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, HostListener, Input, ViewChild, inject } from '@angular/core';
 import { BooleanValueService } from '../../../../../services/boolean-value.service';
+import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 
 @Component({
   selector: 'app-reaction-bar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PickerComponent],
   templateUrl: './reaction-bar.component.html',
   styleUrl: './reaction-bar.component.scss'
 })
@@ -13,10 +14,12 @@ export class ReactionBarComponent {
 
   @Input() isOwnMessage: boolean = true;
   @ViewChild('edit') edit: ElementRef | null = null;
+  @ViewChild('emoji') emoji: ElementRef | null = null;
   booleanService = inject(BooleanValueService);
 
 
   viewOption: boolean = false;
+  viewEmojiPicker: boolean = false;
 
 
   showOption(event: MouseEvent) {
@@ -26,15 +29,29 @@ export class ReactionBarComponent {
 
   @HostListener('document:click', ['$event'])
   onclick(event: Event) {
-    if (this.edit && this.edit.nativeElement.contains(event.target)) {
+    if ((this.edit && this.edit.nativeElement && this.edit.nativeElement.contains(event.target)) || 
+        (this.emoji && this.emoji.nativeElement && this.emoji.nativeElement.contains(event.target))) {
       return
     } else {
       this.viewOption = false;
+      this.viewEmojiPicker = false;
     }
   }
+  
 
 
   showThread() {
     this.booleanService.viewThread.set(true);
+  }
+
+
+  showEmojiPicker(event: MouseEvent) {
+    event.stopPropagation();
+    this.viewEmojiPicker = true;
+  }
+
+  // test ab hier
+  addEmoji(event: MouseEvent) {
+    const emoji = document.createElement('span');
   }
 }
