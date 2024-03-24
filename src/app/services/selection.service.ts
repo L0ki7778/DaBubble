265577cbacd96e8c4562ddaf12fa42069a7809ba
PascalGajewski <121040841+PascalGajewski@users.noreply.github.com>
2024-03-24 13:11:22 +0100

    fixed workspace dropdown channels view

--- src/app/services/selection.service.ts
+++ src/app/services/selection.service.ts
@@ -11,13 +11,13 @@ export class SelectionService {
   channels: string[] = [];
   channelIds: string[] = [];
   DMIds: string[] = [];
-  
+
   choosenChatTypeId = new BehaviorSubject<string>('');
   choosenChatTypeId$ = this.choosenChatTypeId.asObservable();
 
-  channelOrDM = new BehaviorSubject <string>('channel');
+  channelOrDM = new BehaviorSubject<string>('channel');
   channelOrDM$ = this.channelOrDM.asObservable();
-  
+
 
   channelsRef: CollectionReference = collection(this.firestore, "channels");
   channelsQuery = query(this.channelsRef);
@@ -29,12 +29,15 @@ export class SelectionService {
 
 
   constructor() {
-    this.unsubChannels = onSnapshot(this.channelsQuery, (querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        this.channels.push(doc.data()['channelName']);
-        this.channelIds.push(doc.id);
-      }); 
-    });
+    this.unsubChannels = onSnapshot(this.channelsQuery,
+      { includeMetadataChanges: true }, (querySnapshot) => {
+        this.channels = [];
+        this.channelIds = [];
+        querySnapshot.forEach((doc) => {
+          this.channels.push(doc.data()['channelName']);
+          this.channelIds.push(doc.id);
+        });
+      });
     this.unsubDM = onSnapshot(this.directMessagesQuery, (querySnapshot) => {
       querySnapshot.forEach((doc) => {
         this.DMIds.push(doc.id);
