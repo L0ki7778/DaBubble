b093b8d025cfea825187909f602f975aa14f14f8
AntonOsipov99 <antonosipov@gmx.de>
2024-04-12 17:57:34 +0200

    fixed bug to show private chat

--- src/app/services/selection.service.ts
+++ src/app/services/selection.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { Firestore, getDocs } from '@angular/fire/firestore';
-import { collection, query, where, onSnapshot, CollectionReference } from "firebase/firestore";
+import { collection, query, where, onSnapshot, CollectionReference, doc, getDoc } from "firebase/firestore";
 import { BehaviorSubject, timestamp } from 'rxjs';
 import { DirectMessagesService } from './direct-messages.service';
 import { AuthService } from './auth.service';
@@ -69,19 +69,29 @@ export class SelectionService {
     });
   }
 
-  getFirstDocumentId() {
+  async getFirstDocumentId() {
+    const loggedInUserId = await this.dmService.getLoggedInUserId();
     if (this.channelIds.length > 0) {
-      console.log('Channel gefunden');
       this.channelOrDM.next('channel');
       this.choosenChatTypeId.next(this.channelIds[0]);
-    }
-    else if (this.dmService.filteredUserNames.length > 0) {
-      console.log('Message gefunden');
+    } else if (this.DMIds.length > 0) {
       this.channelOrDM.next('direct-message');
-      this.choosenChatTypeId.next(this.DMIds[0]);
-    }
-    else {
-      console.log('Keine Channel oder Message gefunden');
+      const directMessageDocRef = doc(this.firestore, 'direct-messages', this.DMIds[0]);
+      const directMessageDoc = await getDoc(directMessageDocRef);
+      if (directMessageDoc.exists()) {
+        const members = directMessageDoc.data()['members'];
+        const otherUserId = members.find((id: any) => id !== loggedInUserId);
+        if (otherUserId) {
+          const otherUserName = await this.dmService.getUserNameById(otherUserId);
+          this.dmService.selectedUserName = otherUserName;
+          this.choosenChatTypeId.next(this.DMIds[0]);
+          this.dmService.loadChatHistory();
+        } else {
+          console.warn('Es wurde keine andere Benutzer-ID in diesem Chat gefunden.');
+        }
+      } else {
+        console.warn('Das Direct-Message-Dokument wurde nicht gefunden.');
+      }
     }
   }
 
