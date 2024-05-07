3e7f8a9b1e63d6f1a90d47fcca4602436fb0f160
Meran Osman <meran-osman@gmx.de>
2024-05-07 12:06:27 +0200

    profile-view current responsive

--- src/app/main-page/overlay/dropdown-menu/dropdown-menu.component.ts
+++ src/app/main-page/overlay/dropdown-menu/dropdown-menu.component.ts
@@ -8,10 +8,10 @@ import { animate, style, transition, trigger } from '@angular/animations';
 export const slideAnimation = trigger('slideAnimation', [
   transition(':enter', [
     style({ transform: 'translateY(100%)' }),
-    animate('200ms', style({ transform: 'translateY(0)' }))
+    animate('150ms', style({ transform: 'translateY(0)' }))
   ]),
   transition(':leave', [
-    animate('200ms', style({ transform: 'translateY(100%)' }))
+    animate('150ms', style({ transform: 'translateY(100%)' }))
   ])
 ]);
 
