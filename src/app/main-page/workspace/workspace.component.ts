9aaa73eda313559cfdc250db0e5046639eefffc3
Meran Osman <meran-osman@gmx.de>
2024-05-09 16:01:53 +0200

    workspace responsive

--- src/app/main-page/workspace/workspace.component.ts
+++ src/app/main-page/workspace/workspace.component.ts
@@ -2,13 +2,15 @@ import { Component, inject } from '@angular/core';
 import { OverlayService } from '../../services/overlay.service';
 import { WorkspaceDropdownComponent } from './workspace-dropdown/workspace-dropdown.component';
 import { WorkspaceHeaderComponent } from './workspace-header/workspace-header.component';
+import { SearchBarComponent } from '../head/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-workspace',
   standalone: true,
   imports: [
     WorkspaceDropdownComponent,
-    WorkspaceHeaderComponent
+    WorkspaceHeaderComponent,
+    SearchBarComponent
   ],
   templateUrl: './workspace.component.html',
   styleUrl: './workspace.component.scss'
