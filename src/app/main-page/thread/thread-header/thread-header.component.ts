30f784109222a3bb6822fba085a423c8196d6c6d
PascalGajewski <121040841+PascalGajewski@users.noreply.github.com>
2024-05-11 21:16:32 +0200

    fixed thread header channel name

--- src/app/main-page/thread/thread-header/thread-header.component.ts
+++ src/app/main-page/thread/thread-header/thread-header.component.ts
@@ -2,6 +2,9 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from '../../main-page.component';
 import { BooleanValueService } from '../../../services/boolean-value.service';
+import { SelectionService } from '../../../services/selection.service';
+import { DocumentSnapshot } from 'firebase/firestore';
+import { Firestore, doc, onSnapshot } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-thread-header',
@@ -16,10 +19,35 @@ import { BooleanValueService } from '../../../services/boolean-value.service';
 export class ThreadHeaderComponent {
 
   booleanService = inject(BooleanValueService);
+  selectionService = inject(SelectionService);
+  firestore = inject(Firestore);
 
+  currentChannelName: string = '';
+  private unsubscribeChannel: (() => void) | undefined;
+
+  constructor() {
+    this.subscribeToChannelsData();
+  }
+
+  subscribeToChannelsData() {
+    if (this.selectionService.choosenChatTypeId.value) {
+      this.unsubscribeChannel = onSnapshot(doc(this.firestore, 'channels', this.selectionService.choosenChatTypeId.value),
+        { includeMetadataChanges: true }, (channel) => {
+          if (channel.exists() && channel.data() && channel.data()['channelName']) {
+            this.currentChannelName = channel.data()['channelName'] as string;
+          }
+        }
+      );
+    }
+  }
 
   hideThread() {
     this.booleanService.toggleViewThread(false);
   }
 
+  ngOnDestroy() {
+    if (this.unsubscribeChannel) {
+      this.unsubscribeChannel();
+    }
+  }
 }
