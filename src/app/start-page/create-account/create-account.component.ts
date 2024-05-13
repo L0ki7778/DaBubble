f5a2b28813dc00f7132d0cdcb6dc054487ea9c42
AntonOsipov99 <antonosipov@gmx.de>
2024-05-13 19:51:55 +0200

    make create account responsive mobile

--- src/app/start-page/create-account/create-account.component.ts
+++ src/app/start-page/create-account/create-account.component.ts
@@ -1,12 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create-account',
   standalone: true,
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, CommonModule],
   templateUrl: './create-account.component.html',
   styleUrl: './create-account.component.scss'
 })
@@ -18,44 +19,71 @@ export class CreateAccountComponent {
   hideCreateContainer: boolean = false;
   showPrivacyPolicy: boolean = false;
   privacyPolicyChecked: boolean = false;
-
+  showReport: boolean = false;
 
 
   constructor(private router: Router) { }
 
+  ngOnInit() {
+    this.checkWidth();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkWidth();
+  }
+
+  checkWidth() {
+    if(window.innerWidth < 650) {
+      this.privacyPolicyChecked = true;
+    } else {
+      this.privacyPolicyChecked = false;
+    }
+  }
+
   async saveVariables() {
-    this.authService.email = this.email;
-    this.authService.password = this.password;
-    this.authService.name = this.name;
-    await this.authService.toggleToChooseProfilePicture();
+  this.authService.email = this.email;
+  const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  if (!emailRegex.test(this.email)) {
+    this.showReport = true;
+    return;
   }
+  this.authService.password = this.password;
+  this.authService.name = this.name;
+  await this.authService.toggleToChooseProfilePicture();
+}
 
-  toggleToLogin() {
-    this.authService.showLogin = true;
-    this.authService.showCreateAccount = false;
-  }
+toggleToLogin() {
+  this.authService.showLogin = true;
+  this.authService.showCreateAccount = false;
+}
 
-  toggleToPrivacyPolicy() {
-    this.hideCreateContainer = true;
-    this.showPrivacyPolicy = true;
-  }
+toggleToPrivacyPolicy() {
+  this.hideCreateContainer = true;
+  this.showPrivacyPolicy = true;
+}
 
-  toggleToCreateAccount() {
-    this.hideCreateContainer = false;
-    this.showPrivacyPolicy = false;
-  }
+toggleToCreateAccount() {
+  this.hideCreateContainer = false;
+  this.showPrivacyPolicy = false;
+}
 
-  preventAnimation() {
-    this.authService.endAnimation = true;
-    this.authService.animation = false;
-    this.authService.showSlideAnimation = false;
-  }
+preventAnimation() {
+  this.authService.endAnimation = true;
+  this.authService.animation = false;
+  this.authService.showSlideAnimation = false;
+}
 
   get isInputEmpty(): boolean {
-    return this.name.trim() === '' || this.email.trim() === '' || this.password.trim() === '';
-  }
+  return this.name.trim() === '' || this.email.trim() === '' || this.password.trim() === '';
+}
 
   get shouldShowNotAllowed(): boolean {
-    return (this.isInputEmpty || !this.privacyPolicyChecked);
-    }
+  return (this.isInputEmpty || !this.privacyPolicyChecked);
+}
+
+hideReport() {
+  this.showReport = false;
+}
+
 }
